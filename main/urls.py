4d85cfdac7c1ad03ed8284798b51ea7d9e5735b3
A-Belfort <rafaelrja2006@gmail.com>
2025-11-28 11:41:31 -0300

    Creat art
    parte da crianção do usuario

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path ('', views.task_list,name='task_list'),
     path('concluidas/',views.task_concluidas,name='task_concluidas'),
     path('pendentes/',views.task_pendentes,name='task_pendentes'),
+    path('create_task',views.task_create,name='task_crate'),
 ]
--- main/views.py
+++ main/views.py
@@ -28,3 +28,36 @@ def task_pendentes(request):
     }
 
     return render(request, 'Task/TASK_LIST.html', context)
+
+
+
+
+
+
+
+
+def task_create(request):
+
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo', "").strip()
+        descricao = request.POST.get('descricao',"").strip()
+        concluida = request.POST.get('concluida', "")== 'on'
+        area = request.POST.get('area',"").strip()
+        prioridade = request.POST.get('prioridade', "").strip()
+        data_limite = request.POST.get('data_limite',"").strip()
+
+        Task_Canil.objects.create(
+            titulo=titulo,
+            descricao=descricao,
+            concluida=concluida,
+            area=area,
+            prioridade=prioridade,
+            data_limite=data_limite
+        )
+
+        return redirect ("task_list")
+
+    context ={
+        'opcoes_prioridade': Task_Canil.Priority.choices,
+    }
+    return render(request, 'Task/task_form.html', context)
